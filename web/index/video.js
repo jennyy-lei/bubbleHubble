85bfe7d51b85fd85529207c9469c22c6cc7c3a21
DaJellyDanish <denesh100@gmail.com>
2020-10-03 19:10:08 -0400

    more graph updates

--- web/index/video.js
+++ web/index/video.js
@@ -3,7 +3,7 @@ function listenToVideo() {
     document.getElementById("video").addEventListener("timeupdate", function() {
         console.log(this.currentTime);
 
-        var currTime = Math.floor((this.currentTime+1)/5)*5;;
-        updateGraph(line1,[1,2,3,4,5,6])
+        var currTime = Math.floor((this.currentTime+1)/5)*5;
+        updateGraph(line1,[1,2,3,4,5,6,7])
     })
 }
