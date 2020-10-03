4b1a83fcce958b347d6dd61e1fa7edcca7f8c8a1
Vivian Liu <v9liu@edu.uwaterloo.ca>
2020-10-03 17:28:44 -0400

    Video updates

--- web/index/video.js
+++ web/index/video.js
@@ -1,7 +1,12 @@
+
 function listenToVideo() {
     document.getElementById("video").addEventListener("timeupdate", function() {
         console.log(this.currentTime);
 
+        var currTime = Math.floor((this.currentTime+1)/5)*5;;
+        console.log(currTime);
+
+        console.log(testVidData[currTime/5]);
         // do something with current time (in seconds)
     })
 }
