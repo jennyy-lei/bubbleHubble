471783fdca58e3803710c16a7f1a01469500f719
Jenny Lei <jenny.h.lei@hotmail.com>
2020-10-03 23:06:02 -0400

    start stream again

--- web/index/video.js
+++ web/index/video.js
@@ -1,42 +1,40 @@
 function callApi() {
-    console.log("calling api!");
+    call(update);
 
     const interval = setInterval(function() {
-        call();
+        call(update);
     }, 3000);
 
-    clearInterval(interval);
-
-    function call() {
+    function call(callback) {
         fetch("http://52.139.37.93:7777/api")
         .then(response => {
-        //     return response.json();
-        // })
-        // .then(json => {
-            console.log(response);
+            return response.json();
+        })
+        .then(json => {
+            callback(json);
         });
+    }
 
-        document.getElementById("camera").addEventListener("timeupdate", function() {
-            console.log(this.currentTime);
+    function update(json) {
+        console.log(json);
 
-            // var currTime = Math.floor((this.currentTime+1)/5)*5;
-            // var currTime = Math.floor(this.currentTime);
-            // console.log(data[currTime]);
-            // var safe = data[currTime]["safe"];
-            // var low = data[currTime]["low"];
-            // var high = data[currTime]["high"];
-            
-            // // console.log(data[currTime]["safe"]);
-            // updateGraph(pie1, [[high, low, safe]]);
-            // // updateGraph(line1,[1,2,3,4,5,6]);
-            // // line1.data.datasets[0].data.push(dataNew[i]);
-            // // line1.data.datasets[1].data.push(dataNew[i]);
-            // // line1.data.datasets[2].data.push(dataNew[i]);
-            // // line1.data.labels
-            // // updateGraph(line1, []);
+        // var currTime = Math.floor((this.currentTime+1)/5)*5;
+        // var currTime = Math.floor(this.currentTime);
+        // console.log(data[currTime]);
+        var safe = json["safe"];
+        var low = json["low"];
+        var high = json["high"];
+        
+        // // console.log(data[currTime]["safe"]);
+        updateGraph(pie1, [[high, low, safe]]);
+        // // updateGraph(line1,[1,2,3,4,5,6]);
+        // // line1.data.datasets[0].data.push(dataNew[i]);
+        // // line1.data.datasets[1].data.push(dataNew[i]);
+        // // line1.data.datasets[2].data.push(dataNew[i]);
+        // // line1.data.labels
+        // // updateGraph(line1, []);
 
-            // document.getElementById('pie1-legend').innerHTML = pie1.generateLegend();
-            // document.getElementById('line1-legend').innerHTML = line1.generateLegend();
-        });
+        // document.getElementById('pie1-legend').innerHTML = pie1.generateLegend();
+        // document.getElementById('line1-legend').innerHTML = line1.generateLegend();
     }
 }
