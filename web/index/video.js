99ac5db0a52c01962bc3981f01b11782fe865973
DaJellyDanish <denesh100@gmail.com>
2020-10-04 00:17:15 -0400

    fininshed line graphs finally i think

--- web/index/video.js
+++ web/index/video.js
@@ -26,12 +26,24 @@ function callApi() {
         var high = json["high"];
         
         // // console.log(data[currTime]["safe"]);
-        updateGraph(pie1, [high, low, safe]);
-        // // updateGraph(line1,[1,2,3,4,5,6]);
+        updateGraph(pie1, [high, low, safe],0);
         // // line1.data.datasets[0].data.push(dataNew[i]);
         // // line1.data.datasets[1].data.push(dataNew[i]);
         // // line1.data.datasets[2].data.push(dataNew[i]);
         // // line1.data.labels
+        console.log(high, low, safe)
+        var temp = line1.data.datasets[0].data
+        temp.push(high)
+        updateGraph(line1,temp,0);
+
+        var temp2 = line1.data.datasets[1].data
+        temp.push(low)
+        updateGraph(line1,temp2,1);
+
+        var temp3 = line1.data.datasets[2].data
+        temp.push(safe)
+        updateGraph(line1,temp3,2);
+
         // // updateGraph(line1, []);
 
         document.getElementById('pie1-legend').innerHTML = pie1.generateLegend();
